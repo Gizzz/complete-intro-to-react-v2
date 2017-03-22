6fefb2004995abf497b44079b65cb3d5fb4a3b03
Gizzz <bulatkhusainov@yandex.ru>
2017-03-22 13:52:04 +0300

    react intro

---
+++ js/ClientApp.js
@@ -0,0 +1,31 @@
+/* global React ReactDOM */
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render() {
+    return (
+      div(null,
+        h1({ style: { color: this.props.color } }, this.props.title)
+      )
+    )
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+
+//replace render method
+var MyFirstComponent = React.createClass({
+  render() {
+    return (
+      div(null,
+        MyTitleFact({ title: 'Props are great!', color: 'rebeccapurple' }),
+        MyTitleFact({ title: 'Use props everywhere!', color: 'mediumaquamarine' }),
+        MyTitleFact({ title: 'Props are the best!', color: 'peru' })
+      )
+    )
+  }
+})
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
