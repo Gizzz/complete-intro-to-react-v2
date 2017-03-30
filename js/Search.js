5626bc0f16ff9e64581928fc4a27fa0a8c19b7ff
Gizzz <bulatkhusainov@yandex.ru>
2017-03-30 12:54:57 +0300

    add live search to search component

--- js/Search.js
+++ js/Search.js
@@ -4,16 +4,31 @@ import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is the default searchTerm'
+    }
+  },
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  },
   render () {
     return (
       <div className='search'>
+        <header>
+          <h1>{this.state.searchTerm}</h1>
+          <input type='text' placeholder='Search' 
+            value={this.state.searchTerm} 
+            onChange={this.handleSearchTermChange} 
+          />
+        </header>
         <div>
           {
-            preload.shows.map((show) => {
-              return (
-                <ShowCard {...show} key={show.imdbID} />
-              )
-            })
+            preload.shows
+              .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+              .map((show, index) => (
+                <ShowCard {...show} key={index} id={index} />
+              ))
           }
         </div>
       </div>
