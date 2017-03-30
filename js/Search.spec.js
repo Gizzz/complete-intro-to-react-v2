655940cbb0f906ada16ef7e10933726c017e95da
Gizzz <bulatkhusainov@yandex.ru>
2017-03-30 17:38:21 +0300

    add Enzyme (testing) to the mix; write some tests for Search.js

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,9 +1,29 @@
 import React from 'react'
-import Search from './Search'
-import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
 
-test('Search should search titles', () => {
-  const component = renderer.create(<Search />)
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+import Search from './Search'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
+
+test('Search render correctly', () => {
+  const component = shallow(<Search />)
+	const tree = shallowToJson(component)
+	expect(tree).toMatchSnapshot()
+})
+
+test('Search should render a ShowCard for each show', () => {
+	const component = shallow(<Search />)
+	expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+})
+
+test('Search should render correct amount of shows based on search', () => {
+	const searchWord = 'house'
+	const component = shallow(<Search />)
+	component
+		.find('input')
+		.simulate('change', { target: { value: searchWord } })
+	const showCount = preload.shows
+		.filter((show) => `${show.title.toUpperCase()} ${show.description.toUpperCase()}`.includes(searchWord.toUpperCase())).length
+	expect(showCount).toEqual(component.find(ShowCard).length)
 })
