09f07c0b6a7aebf3d9a8983012b43edf0f2361f7
Gizzz <bulatkhusainov@yandex.ru>
2017-03-31 18:22:39 +0300

    add rating fetching via axios

--- js/Details.js
+++ js/Details.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import axios from 'axios'
+
 import Header from './Header'
+
 const { shape, string } = React.PropTypes
 
 const Details = React.createClass({
@@ -9,11 +12,34 @@ const Details = React.createClass({
       year: string,
       poster: string,
       trailer: string,
-      description: string
+      description: string,
+			imdbID: string
     })
   },
+	getInitialState () {
+		return {
+			omdbData: {}
+		}
+	},
+	componentDidMount () {
+		axios.get(`http://www.omdbapi.com/?i=${this.props.show.imdbID}`)
+			.then((response) => {
+				console.log('response', response)
+				this.setState({omdbData: response.data})
+			})
+			.catch((error) => {
+				console.error('axios error', error)
+			})
+	},
   render () {
 		const { title, description, year, poster, trailer } = this.props.show
+
+		let rating
+		if (this.state.omdbData.imdbRating) {
+			rating = <h3>{this.state.omdbData.imdbRating}</h3>
+		} else {
+			rating = <img src='/public/img/loading.png' alt='loading indicator' />
+		}
 
     return (
       <div className='details'>
@@ -21,6 +47,7 @@ const Details = React.createClass({
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+					{rating}
           <img src={`/public/img/posters/${poster}`} />
           <p>{description}</p>
         </section>
