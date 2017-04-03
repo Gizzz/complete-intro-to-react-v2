d731eb722d348f7741e08be905c9bc43f24a8306
Gizzz <bulatkhusainov@yandex.ru>
2017-04-03 17:04:49 +0300

    integrate redux to app, router

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Match } from 'react-router'
+import { Provider } from 'react-redux'
+
+import store from './store'
 
 import Landing from './Landing'
 import Search from './Search'
@@ -15,20 +18,22 @@ const App = React.createClass({
   render () {
     return (
       <BrowserRouter>
-        <div className='app'>
-          <Match exactly pattern='/' component={Landing} />
-          <Match
-            pattern='/search'
-            component={(props) => <Search shows={preload.shows} {...props} />}
-          />
-          <Match
-            pattern='/details/:id'
-            component={(props) => {
-              const show = preload.shows.filter((show) => props.params.id === show.imdbID)
-              return <Details show={show[0]} {...props} />
-            }}
-          />
-        </div>
+        <Provider store={store}>
+          <div className='app'>
+            <Match exactly pattern='/' component={Landing} />
+            <Match
+              pattern='/search'
+              component={(props) => <Search shows={preload.shows} {...props} />}
+            />
+            <Match
+              pattern='/details/:id'
+              component={(props) => {
+                const show = preload.shows.filter((show) => props.params.id === show.imdbID)
+                return <Details show={show[0]} {...props} />
+              }}
+            />
+          </div>
+        </Provider>
       </BrowserRouter>
     )
   }
