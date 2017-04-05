0227266699ef4aa04c946d1de03972aa1199cfe3
Gizzz <bulatkhusainov@yandex.ru>
2017-04-05 13:55:24 +0300

    update tests to use redux, add reducer tests

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,29 +1,29 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { Provider } from 'react-redux'
+import { shallow, render } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
 
-import Search from './Search'
+import store from './store'
+import { setSearchTerm } from './actionCreators'
+import Search, { Unwrapped as UnwrappedSearch } from './Search'
 import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 test('Search render correctly', () => {
-  const component = shallow(<Search />)
+  const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm='' />)
 	const tree = shallowToJson(component)
 	expect(tree).toMatchSnapshot()
 })
 
 test('Search should render a ShowCard for each show', () => {
-	const component = shallow(<Search />)
+	const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm='' />)
 	expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
 
 test('Search should render correct amount of shows based on search', () => {
-	const searchWord = 'house'
-	const component = shallow(<Search />)
-	component
-		.find('input')
-		.simulate('change', { target: { value: searchWord } })
-	const showCount = preload.shows
-		.filter((show) => `${show.title.toUpperCase()} ${show.description.toUpperCase()}`.includes(searchWord.toUpperCase())).length
-	expect(showCount).toEqual(component.find(ShowCard).length)
+  const searchWord = 'house'
+  store.dispatch(setSearchTerm(searchWord))
+  const component = render(<Provider store={store}><Search shows={preload.shows} /></Provider>)
+  const showCount = preload.shows.filter((show) => `${show.title.toUpperCase()} ${show.description.toUpperCase()}`.includes(searchWord.toUpperCase())).length
+  expect(showCount).toEqual(component.find('.show-card').length)
 })
